b763c100c7e7d90da828c0c24f907c8fb37af2a2
Henry Allen <hmallen99@gmail.com>
2024-05-05 23:13:19 -0400

    Add test for children

--- packages/pipe/src/index.spec.ts
+++ packages/pipe/src/index.spec.ts
@@ -3,7 +3,7 @@ import { createRoot } from './createRoot';
 import { Observable, Subject, BehaviorSubject, scan, map, tap } from 'rxjs';
 
 describe('Pipe', () => {
-    it('should render a single child', () => {
+    it('should render an HTML element in a component', () => {
         const Counter: Component<Record<string, Observable<void>>> = () => {
             const click$ = new Subject<void>();
             const onclick = new BehaviorSubject<() => void>(() => {
@@ -81,6 +81,74 @@ describe('Pipe', () => {
         unmount();
 
         expect(container.firstChild).toBeFalsy();
+    });
+
+    it('should nest children in a pipe element', () => {
+        const spy = jest.fn();
+        const Text: Component<{
+            count$: Observable<number>;
+        }> = ({ count$ }) => {
+            const textContent = count$.pipe(
+                map((x) => String(x)),
+                tap((value) => spy(value))
+            );
+
+            return createElement('p', {
+                textContent,
+            });
+        };
+
+        const TextWrapper: Component<{
+            count$: Observable<number>;
+        }> = ({ count$ }) => {
+            return createElement('div', {}, [
+                createElement(Text, { count$ }),
+                createElement(Text, { count$: count$.pipe(map((x) => x * 2)) }),
+            ]);
+        };
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const count$ = new Subject<number>();
+
+        render(
+            createElement(TextWrapper, {
+                count$,
+            })
+        );
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeInstanceOf(HTMLDivElement);
+
+        const singleText = div.firstChild;
+        const doubleText = singleText.nextSibling;
+
+        expect(singleText.textContent).toEqual('');
+        expect(doubleText.textContent).toEqual('');
+
+        count$.next(1);
+
+        expect(singleText.textContent).toEqual('1');
+        expect(doubleText.textContent).toEqual('2');
+        expect(spy).toHaveBeenCalledTimes(2);
+        expect(spy).toHaveBeenCalledWith('1');
+        expect(spy).toHaveBeenCalledWith('2');
+
+        count$.next(2);
+
+        expect(singleText.textContent).toEqual('2');
+        expect(doubleText.textContent).toEqual('4');
+        expect(spy).toHaveBeenCalledTimes(4);
+        expect(spy).toHaveBeenCalledWith('2');
+        expect(spy).toHaveBeenCalledWith('4');
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
+        count$.next(3);
+
+        expect(spy).toHaveBeenCalledTimes(4);
     });
 
     it('should clean up observables on unmount', () => {
