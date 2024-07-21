8244ea82e920e36236ee644cc47c07db694d72d3
Henry Allen <hmallen99@gmail.com>
2024-07-21 12:54:10 -0400

    Remove log

--- packages/pipe/src/createElement.ts
+++ packages/pipe/src/createElement.ts
@@ -25,7 +25,6 @@ export function createElement<
 
     cleanup$.subscribe({
         complete: () => {
-            console.log(element.textContent);
             element.remove();
         },
     });
