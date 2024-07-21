cbb38f37bfc2dc41b76fe3e793fe3cf7f2a86d6d
Henry Allen <hmallen99@gmail.com>
2024-07-21 13:45:55 -0400

    Add tests

--- packages/pipe/src/index.spec.ts
+++ packages/pipe/src/index.spec.ts
@@ -1,6 +1,14 @@
 import { Component, createElement } from './createElement';
 import { createRoot } from './createRoot';
-import { Observable, Subject, BehaviorSubject, scan, map, tap } from 'rxjs';
+import {
+    Observable,
+    Subject,
+    BehaviorSubject,
+    scan,
+    map,
+    tap,
+    distinctUntilChanged,
+} from 'rxjs';
 
 describe('Pipe', () => {
     it('should render an HTML element in a component', () => {
@@ -272,5 +280,229 @@ describe('Pipe', () => {
 
         expect(spy).toHaveBeenCalledTimes(2);
         expect(spy).toHaveBeenCalledWith('2');
+    });
+});
+
+describe('Boolean Operator', () => {
+    const SwitchComponent: Component<{
+        bool$: Observable<boolean>;
+    }> = ({ bool$ }) => {
+        return createElement(
+            'div',
+            {},
+            bool$.pipe(
+                map((value) => [
+                    'default',
+                    value
+                        ? createElement('p', {
+                              textContent: new BehaviorSubject('true'),
+                          })
+                        : createElement('p', {
+                              textContent: new BehaviorSubject('false'),
+                          }),
+                ])
+            )
+        );
+    };
+
+    const CountListenerComponent: Component<{ count$: Observable<number> }> = ({
+        count$,
+    }) => {
+        return createElement('p', {
+            textContent: count$,
+        });
+    };
+
+    const ComplexSwitchComponent: Component<{
+        bool$: Observable<boolean>;
+        count$: Observable<number>;
+    }> = ({ bool$, count$ }) => {
+        return createElement(
+            'div',
+            {},
+            bool$.pipe(
+                distinctUntilChanged(),
+                map((value) => [
+                    'default',
+                    value
+                        ? createElement(CountListenerComponent, { count$ })
+                        : createElement(CountListenerComponent, {
+                              count$: count$.pipe(map((count) => count * 2)),
+                          }),
+                ])
+            )
+        );
+    };
+
+    it('should not mount a node on initialization', () => {
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const bool$ = new Subject<boolean>();
+
+        render(createElement(SwitchComponent, { bool$ }));
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeTruthy();
+        expect(div.firstChild).toBeFalsy();
+
+        bool$.next(true);
+
+        const truthyText = div.firstChild as HTMLParagraphElement;
+        expect(truthyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(truthyText.textContent).toEqual('true');
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
+    });
+
+    it('should switch to the falsy value when the bool is updated', () => {
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const bool$ = new Subject<boolean>();
+
+        render(createElement(SwitchComponent, { bool$ }));
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeTruthy();
+        expect(div.firstChild).toBeFalsy();
+
+        bool$.next(true);
+
+        const truthyText = div.firstChild as HTMLParagraphElement;
+        expect(truthyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(truthyText.textContent).toEqual('true');
+
+        expect(div.childNodes.length).toEqual(1);
+
+        bool$.next(false);
+
+        const falsyText = div.firstChild as HTMLParagraphElement;
+        expect(falsyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(falsyText.textContent).toEqual('false');
+
+        expect(div.childNodes.length).toEqual(1);
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
+    });
+
+    it('should switch between truthy and falsy values', () => {
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const bool$ = new Subject<boolean>();
+
+        render(createElement(SwitchComponent, { bool$ }));
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeTruthy();
+        expect(div.firstChild).toBeFalsy();
+
+        bool$.next(true);
+
+        const truthyText = div.firstChild as HTMLParagraphElement;
+        expect(truthyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(truthyText.textContent).toEqual('true');
+
+        expect(div.childNodes.length).toEqual(1);
+
+        bool$.next(false);
+
+        const falsyText = div.firstChild as HTMLParagraphElement;
+        expect(falsyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(falsyText.textContent).toEqual('false');
+
+        expect(div.childNodes.length).toEqual(1);
+
+        bool$.next(true);
+
+        const truthyText2 = div.firstChild as HTMLParagraphElement;
+        expect(truthyText2).toBeInstanceOf(HTMLParagraphElement);
+        expect(truthyText2.textContent).toEqual('true');
+
+        expect(div.childNodes.length).toEqual(1);
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
+    });
+
+    it('should not re-create the child if the boolean does not change', () => {
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const bool$ = new Subject<boolean>();
+
+        render(createElement(SwitchComponent, { bool$ }));
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeTruthy();
+        expect(div.firstChild).toBeFalsy();
+
+        bool$.next(true);
+
+        const truthyText = div.firstChild as HTMLParagraphElement;
+        expect(truthyText).toBeInstanceOf(HTMLParagraphElement);
+        expect(truthyText.textContent).toEqual('true');
+
+        bool$.next(true);
+
+        expect(div.firstChild).toEqual(truthyText);
+
+        bool$.next(false);
+
+        expect(div.firstChild).not.toEqual(truthyText);
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
+    });
+
+    it('should render components with children', () => {
+        const container = document.createElement('div');
+
+        const { render, unmount } = createRoot(container);
+
+        const bool$ = new Subject<boolean>();
+        const count$ = new BehaviorSubject(0);
+
+        render(createElement(ComplexSwitchComponent, { bool$, count$ }));
+
+        const div = container.firstChild as HTMLDivElement;
+        expect(div).toBeInstanceOf(HTMLDivElement);
+
+        bool$.next(true);
+
+        const singleCount = div.firstChild as HTMLParagraphElement;
+        expect(singleCount.textContent).toEqual('0');
+
+        count$.next(1);
+
+        expect(singleCount.textContent).toEqual('1');
+
+        expect(container.childNodes.length).toEqual(1);
+
+        bool$.next(false);
+
+        const doubleCount = div.firstChild as HTMLParagraphElement;
+        expect(doubleCount.textContent).toEqual('2');
+
+        count$.next(2);
+
+        expect(doubleCount.textContent).toEqual('4');
+
+        expect(container.childNodes.length).toEqual(1);
+
+        unmount();
+
+        expect(container.firstChild).toBeFalsy();
     });
 });
