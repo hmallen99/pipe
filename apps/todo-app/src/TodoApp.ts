56efb97483a9d1b3075aadcb0d7cd47f139b1ff8
Henry Allen <hmallen99@gmail.com>
2024-10-12 16:31:12 -0400

    Add todo app

---
+++ apps/todo-app/src/TodoApp.ts
@@ -0,0 +1,75 @@
+import {
+  BehaviorSubject,
+  Component,
+  createElement,
+  map,
+  merge,
+  Observable,
+  of,
+  PipeNode,
+  scan,
+  Subject,
+} from '@pipe/pipe';
+
+export const TodoApp: Component<Record<string, Observable<void>>> = () => {
+  const clickAdd$ = new Subject<string>();
+  const clickRemove$ = new Subject<string>();
+
+  const addChild$: Observable<[string, PipeNode]> = clickAdd$.pipe(
+    scan(({ count }, todoText) => ({ count: count + 1, todoText }), {
+      count: 0,
+      todoText: '' as string,
+    }),
+    map(({ count, todoText }) => {
+      const key = String(count);
+      return [
+        key,
+        createElement(TodoItem, {
+          onRemove: of(() => {
+            clickRemove$.next(key);
+          }),
+          text: of(todoText),
+        }),
+      ];
+    }),
+  );
+
+  const removeChild$: Observable<[string, null]> = clickRemove$.pipe(
+    map((key) => [key, null]),
+  );
+
+  return createElement('div', {}, [
+    createElement('div', {}, merge(addChild$, removeChild$)),
+    createElement(
+      'form',
+      {
+        onsubmit: of((e: Event) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const todoText = formData.get('todo');
+          clickAdd$.next(todoText as string);
+        }),
+      },
+      [
+        createElement('input', {
+          type: of('text'),
+          name: of('todo'),
+        }),
+        createElement('input', {
+          type: of('submit'),
+          value: of('Add TODO'),
+        }),
+      ],
+    ),
+  ]);
+};
+
+export const TodoItem: Component<{
+  onRemove: Observable<() => void>;
+  text: Observable<string>;
+}> = ({ onRemove, text }) => {
+  return createElement('div', {}, [
+    createElement('button', { textContent: of('X'), onclick: onRemove }),
+    createElement('p', { textContent: text }),
+  ]);
+};
--- apps/todo-app/src/index.ts
+++ apps/todo-app/src/index.ts
@@ -1,34 +1,10 @@
-import {
-  BehaviorSubject,
-  Component,
-  createElement,
-  createRoot,
-  map,
-  Observable,
-  scan,
-  Subject,
-} from '@pipe/pipe';
+import { createElement, createRoot } from '@pipe/pipe';
 
 import './index.css';
+import { TodoApp } from './TodoApp';
 
 const rootElement = document.querySelector<HTMLDivElement>('#root')!;
 
 const root = createRoot(rootElement);
 
-const Counter: Component<Record<string, Observable<void>>> = () => {
-  const click$ = new Subject<void>();
-  const onclick = new BehaviorSubject<() => void>(() => {
-    click$.next();
-  });
-  const textContent = click$.pipe(
-    scan((x) => x + 1, 0),
-    map((x) => String(x)),
-  );
-
-  return createElement('button', {
-    onclick,
-    textContent,
-  });
-};
-
-root.render(createElement(Counter, {}));
+root.render(createElement(TodoApp, {}));
