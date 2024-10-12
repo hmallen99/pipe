58be3d409187630c1df0d2d3d5f96e5a6426532c
Henry Allen <hmallen99@gmail.com>
2024-10-12 15:49:34 -0400

    Add basic element

--- apps/todo-app/src/index.ts
+++ apps/todo-app/src/index.ts
@@ -1,10 +1,34 @@
-import { createRoot } from '@pipe/pipe';
+import {
+  BehaviorSubject,
+  Component,
+  createElement,
+  createRoot,
+  map,
+  Observable,
+  scan,
+  Subject,
+} from '@pipe/pipe';
 
 import './index.css';
 
-document.querySelector('#root')!.innerHTML = `
-<div class="content">
-  <h1>Vanilla Rsbuild</h1>
-  <p>Start building amazing things with Rsbuild.</p>
-</div>
-`;
+const rootElement = document.querySelector<HTMLDivElement>('#root')!;
+
+const root = createRoot(rootElement);
+
+const Counter: Component<Record<string, Observable<void>>> = () => {
+  const click$ = new Subject<void>();
+  const onclick = new BehaviorSubject<() => void>(() => {
+    click$.next();
+  });
+  const textContent = click$.pipe(
+    scan((x) => x + 1, 0),
+    map((x) => String(x)),
+  );
+
+  return createElement('button', {
+    onclick,
+    textContent,
+  });
+};
+
+root.render(createElement(Counter, {}));
